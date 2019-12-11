6506a47535a4c44480facea85e1a41779cff871b
PatrykMandrak <patrykmandrak123@gmail.com>
2019-12-11 03:47:33 +0100

    Code refactor

--- Resources/JavaScript/core.js
+++ Resources/JavaScript/core.js
@@ -546,7 +546,7 @@ window.smoothScroll = function(target) {
         scrollContainer.scrollTop += 1;
     } while (scrollContainer.scrollTop === 0);
 
-    var targetY = 0 - 25;
+    var targetY = 0 - 18;
     do { //find the top of target relatively to the container
         if (target === scrollContainer) break;
         targetY += target.offsetTop;
